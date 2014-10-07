5656f0e652479e13bdeb888930c0d9b59d7890f8
Baptiste Costa <baptiste@sefaireaider.com>
2014-10-07 18:01:37 +0200

    - Tennis place finder.

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
-
 namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
@@ -16,6 +8,9 @@ class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
+//        $this->getServiceLocator()->get('tennis-place-finder')->run();
+
+
         return new ViewModel();
     }
 }
--- module/Application/src/Application/Service/TennisPlaceFinder.php
+++ module/Application/src/Application/Service/TennisPlaceFinder.php
@@ -1,6 +1,168 @@
 <?php
+namespace Application\Service;
+use Api\V1\Rest\Place\PlaceMapper;
+use SebastianBergmann\Exporter\Exception;
+use Zend\Http\Client;
+use Zend\Http\Header\Accept;
+use Zend\Http\Request;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\ServiceManager\ServiceLocatorAwareTrait;
+use Zend\Stdlib\Parameters;
 
-class TennisPlaceFinder
+/**
+ * Class TennisPlaceFinder
+ */
+class TennisPlaceFinder implements ServiceLocatorAwareInterface
 {
+    use ServiceLocatorAwareTrait;
 
-} 
+    protected $location;
+
+    protected $placeTypes = [];
+
+    protected $tennisPlaces = [];
+
+    /**
+     * @var Client
+     */
+    protected $httpClient;
+
+    function __construct()
+    {
+        $this->httpClient = new Client(null, [
+            'sslverifypeer' => false
+        ]);
+    }
+
+    public function run($location)
+    {
+        if (empty($location)) {
+            throw new Exception('Invalid location');
+        }
+        $this->location = $location;
+
+        $this->requestPlaceTypes();
+        $this->requestTennisPlaces();
+        $this->saveTennisPlaces();
+    }
+
+    private function requestPlaceTypes()
+    {
+        $req = $this->getPlaceTypesRequest();
+        $res = $this->sendHttpRequest($req);
+        $data = $this->unserializeJsonData($res->getBody());
+
+        $placeTypes = $data['_embedded']['place_type'];
+
+        foreach ($placeTypes as $placeType) {
+            $this->placeTypes[$placeType['name']] = $placeType['id'];
+        }
+    }
+
+    private function requestTennisPlaces($token = null)
+    {
+        if ($token) {
+            $req = $this->getSearchNearbyPlaceWithTokenRequest($token);
+        } else {
+            $req = $this->getSearchNearbyPlaceRequest();
+        }
+        $response = $this->sendHttpRequest($req);
+        $data = $this->unserializeJsonData($response->getBody());
+        $this->assertStatusIsOk($data['status']);
+
+        $this->onTennisPlacesFound($data['results']);
+
+        static $shield = 0;
+        if (!empty($data['next_page_token']) && $shield++ <= 10) {
+            sleep(5);
+            $this->requestTennisPlaces($data['next_page_token']);
+        }
+    }
+
+    private function saveTennisPlaces()
+    {
+        /** @var PlaceMapper $placeMapper */
+        $placeMapper = $this->getServiceLocator()->get('Api\\V1\\Rest\\Place\\PlaceMapper');
+
+        foreach ($this->tennisPlaces as $tennisPlace) {
+            $placeMapper->create($tennisPlace);
+        }
+    }
+
+    private function onTennisPlacesFound($places)
+    {
+        foreach ($places as $place) {
+            $this->tennisPlaces[] = [
+                'name' => $place['name'],
+                'latitude' => $place['geometry']['location']['lat'],
+                'longitude' => $place['geometry']['location']['lng'],
+                'types' => $this->getPlaceTypesIdsFromName($place['types'])
+            ];
+        }
+    }
+
+    private function getPlaceTypesIdsFromName($placeTypeNames)
+    {
+        $types = [];
+        foreach ($placeTypeNames as $name) {
+            $types[] = $this->placeTypes[$name];
+        }
+        return $types;
+    }
+
+    private function sendHttpRequest(Request $request)
+    {
+        return $this->httpClient->send($request);
+    }
+
+    private function getPlaceTypesRequest()
+    {
+        $acceptHeader = new Accept();
+        $acceptHeader->addMediaType('application/json');
+
+        $req = new Request();
+        $req->setUri('http://mon-partenaire.loc/api/place-type');
+        $req->getHeaders()->addHeader($acceptHeader);
+        return $req;
+    }
+
+    private function getSearchNearbyPlaceWithTokenRequest($token)
+    {
+        $req = new Request();
+        $req->setUri('https://maps.googleapis.com/maps/api/place/nearbysearch/json');
+        $req->setMethod(Request::METHOD_GET);
+        $req->setQuery(new Parameters([
+            'key' => 'AIzaSyDbJurRkujBxXxNxCNDW1LJ3c9HtQJ6yY8',
+            'pagetoken' => $token
+        ]));
+        return $req;
+    }
+
+    private function getSearchNearbyPlaceRequest()
+    {
+        $req = new Request();
+        $req->setUri('https://maps.googleapis.com/maps/api/place/nearbysearch/json');
+        $req->setMethod(Request::METHOD_GET);
+        $req->setQuery(new Parameters([
+            'key' => 'AIzaSyDbJurRkujBxXxNxCNDW1LJ3c9HtQJ6yY8',
+            'radius' => 50000,
+            'name' => 'tennis',
+            'keyword' => 'tennis',
+            'language' => 'fr',
+            'location' => $this->location
+        ]));
+        return $req;
+    }
+
+    public function assertStatusIsOk($status)
+    {
+        if ($status != 'OK') {
+            throw new Exception($status);
+        }
+    }
+
+    public function unserializeJsonData($response)
+    {
+        return json_decode($response, true);
+    }
+}
